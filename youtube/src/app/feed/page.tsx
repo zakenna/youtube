ba78478dc7c6f97e0b673a547b1edf1aa7b434a6
leeyeonwoong <(uc2412030@gmail.com)>
2025-06-26 10:47:19 +0900

    환경 셋팅

---
+++ youtube/src/app/feed/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div>
+          feed page!
+        </div>
+    );
+};
+
+export default page;
---
+++ youtube/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import { Radius } from "lucide-react";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+     <Image src="/logo.svg" height={50} width={50} alt="logo"/>
+     <p>youtube</p>
+    </div>
+  );
+}
